c791f7a102b50afb7e1af8eca3ab85b1c0cdfee1
Mona Lisa <45538642+livrth@users.noreply.github.com>
2022-06-07 19:24:29 +0800

    Fix DFS & Add case

--- README.md
+++ README.md
@@ -6,54 +6,24 @@ Team Member:
 - [@R-oink](https://github.com/R-oink)
 - [@tempure](https://github.com/tempure)
 
+Compile & Run:
+```bash
+g++ main.cpp -std=c++14 -fexec-charset=GBK -o test
+
+./test
+```
+
 6/3 TODO:
 - [x] Review the CFG Simplification
-- [ ] Try to remove useless symbol
-
---- 
-
-__Some ideas and problems encountered__
-
-Example Input:
-```
-9
-S->a|bA|B|ccD|Pc
-A->abB|epsilon|P
-P->ppP|A
-B->aA
-C->ddC
-D->ddd
-E->eE|F
-F->fF|G
-G->gG
-```
-
-先从 S 开始 DFS, 考虑 S 右边的所有项中的大写字母, 比如 ccD 只考虑 D
-- DFS 右边这些大写字母, 去掉所有DFS完这些大写字母都不可达的大写字符集 E F G
-- 只需要判断是大写字符是否可达, 某个大写字符不可达, 就要删除所有含有这个大写字符的产生式
-- 枚举每个产生式然后判断是否在其他产生式右边出现过思路是错的, E F G 这三项就可以证明
-
-考虑图中有环的特殊情况, 上述样例中 A P 的关系:
-- 假设先搜索到 A, 然后标记 A 为搜索过,然后 A 出发搜索邻接点 P, 标记 P 为搜索过, 从 P 出发遇到 A 发现已经搜过就不再搜 A, 然后考虑 P->ppP 这个递归式，这个式子是不可达的，要特殊判断递归式的格式，包括左递归和右递归。搜索完 P 的所有项了递归返回结果是 P 为不可达状态。如果不标记A是否搜过，再去搜 A 就会陷入死循环。
-
-- 直接将 A 代入 P 产生式得到 P->ppP|abB|epsilon|P, 那么此时P->epsilon 是可达的, 但是如何处理 P->P, 以及如何用代码实现“代入” ?
-
-到底 A 和 P 的推导关系该如何处理才能得到正确结果, 同时保证 DFS 不会无限递归陷入死循环?
-
---- 
+- [x] Think how to use graph theory to solve it
+- [x] Try to remove useless symbol
 
 
 __6.6 Update__
-```
-A -> B | C | D
-B -> bB | E | F
-E -> eE | c
-F -> eF | G
-G -> gG | b
-```
-每一项大写字母都DFS 任意一项可达就是可达的
 
-有环情况:
+每一个起始符号可达的起始字符字母都进行 DFS,任意一项可达就是可达的。
+
+考虑有环型关系推导的情况:
 ```
 A -> B | C | D | c
 C -> A | E
@@ -61,8 +31,8 @@ B -> bB
 D -> dD 
 E -> eE
 ```
-A 是不可达的 无用式子
-防止环出现就搜过的不再搜了, 而且遇到环的话返回是 false
+A 是不可达的无用式子,防止环出现就搜过的不再搜了, 而且遇到环的话返回是 false
+
 举例: A -> C, C -> A | E
 
 此时 C -> A 发现 A 已被标记搜过，那么这一项 DFS(A)  = false
@@ -73,7 +43,54 @@ A -> A | B | C 这种应该化简为 A -> B | C, A->A 是要去掉的
 
 如果只有 A -> A, 那么A就无法到达结束符，A 也是无用式子
 
----
+DFS 搜索可达性, 测试样例1:
+```
+9
+S A P B C D E F G
+9
+S->a | bA| B | ccD | Pc
+A->abB | epsilon | P
+P->ppP | A | C | E
+B->aA
+C->ddC | D
+D->ddd
+E->eE | F
+F->fF | G
+G->gG
+```
+输出 DFS 之后的可达性结果:
+```
+S 1
+A 1
+P 1
+B 1
+C 1
+D 1
+E 0
+F 0
+G 0
+```
+测试样例 2:
+```
+6
+S A B C D E
+6
+S -> A | B | C
+A -> Bd | Cf | D | c
+C -> A | E
+B -> bB
+D -> dD 
+E -> eE
+```
+输出 DFS 之后的可达性结果:
+```
+S 1
+A 1
+B 0
+C 1
+D 0
+E 0
+```
 
 __Reference__
 
--- main.cpp
+++ main.cpp
@@ -19,12 +19,17 @@ D->ddd
 */
 
 const int N = 50;
+int h[N], e[N], idx, ne[N];
+bool vis[N];
+bool reachable[N];
+map<string, vector<string> > all_production;
+map<string, int> start_symbol;
+map<int, string> start_symbol_rev;
+int number = 1;
 
-struct Node {
-    string val;              //字符串值
-    int number;              //点编号
-    bool reachable = false;  //能否到达终结符
-} node[N];
+void add(int a, int b) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
 
 void out_dbg(map<string, vector<string> > &all_production) {
     puts("-------------------------------------------");
@@ -36,48 +41,77 @@ void out_dbg(map<string, vector<string> > &all_production) {
     }
 }
 
-int h[N], e[N], idx, ne[N];
-bool vis[N];
+bool dfs(int u) {  //判断点 u 开始能否到达终结符
+    if (reachable[u]) return true;
+    //u右边有纯终结符组成的字符串返回 true
+    auto vec = all_production[start_symbol_rev[u]];
+    // puts("------------------------------");
+    // cout << start_symbol_rev[u] << endl;
+    // puts("------------------------------");
+    for (auto t : vec) {
+        bool flag = true;
+        for (auto c : t) {
+            if (c >= 'A' && c <= 'Z') {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            reachable[u] = true;
+            break;
+        }
+    }
 
-void add(int a, int b) {
-    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
-}
-
-void dfs(int u) {  //有向图不用考虑反向边
-    vis[u] = true;
+    // u能到的起始符也是可达的返回 true
+    // A -> B | C
     for (int i = h[u]; ~i; i = ne[i]) {
         int j = e[i];
-        if (vis[j]) continue;
-        node[j].reachable = true;
-        vis[j] = true;
-        dfs(j);
+        // cout << start_symbol_rev[j] << ' ';
+        // if (vis[j]) continue;
+        // vis[j] = true;
+        reachable[u]  |= dfs(j);
     }
+    if (reachable[u]) return true;
+
+    return false; //有环的情况 A->C, C->A => A->A 也是不可达的
 }
 
 void solve() {
     memset(h, -1, sizeof h);  //邻接表初始化
+    cout << "\n请输入起始符号数量: ";
+    int cnt;
+    cin >> cnt;
+    cout << "\n请输入所有起始符: ";
+    for (int i = 0; i < cnt; i++) {
+        string key;
+        cin >> key;
+        start_symbol.insert({key, number});
+        start_symbol_rev.insert({number, key});
+        number++;
+    }
     int n;
     cout << "\n请输入产生式的数量: ";
     cin >> n;
-    cout << "\n请输入所有产生式, 每个一行, 空字符使用 epsilon 替代: \n\n";
+    cout << "\n请输入所有产生式, 确保文法最初从起始符 S 开始, 每个一行, 空字符使用 epsilon 替代: \n\n";
     getchar();
 
-    map<string, vector<string> > all_production;
-    int number = 1;
-    map<string, int> state_number;
     for (int i = 0; i < n; i++) {
         string s;
         getline(cin, s);
         string key = s.substr(0, 1);
-        if (state_number.find(key) == state_number.end()) state_number[key] = number++;
+        // if (start_symbol.find(key) == start_symbol.end()) { //两个互相映射
+
         string temp = "";
         for (int i = 1; s[i]; i++) {
             char c = s[i];
             if (c == '-' || c == '>' || isspace(c)) continue;
             if (c == '|') {
                 all_production[key].push_back(temp);
-                if (state_number.find(temp) == state_number.end()) state_number[temp] = number++;
-                add(state_number[temp], state_number[key]);
+                for (auto s : temp) {
+                    if (s >= 'A' && s <= 'Z' && string(1, s) != key) {
+                        add(start_symbol[key], start_symbol[string(1, s)]);
+                    }
+                }
                 temp = "";
                 continue;
             }
@@ -85,61 +119,42 @@ void solve() {
         }
         if (temp.size() != 0) {
             all_production[key].push_back(temp);
-            if (state_number.find(temp) == state_number.end()) state_number[temp] = number++;
-            add(state_number[temp], state_number[key]);
+            for (auto s : temp) {
+                if (s >= 'A' && s <= 'Z' && string(1, s) != key) {
+                    add(start_symbol[key], start_symbol[string(1, s)]);
+                }
+            }
         }
     }
     // out_dbg(all_production);
+    // for (auto t : start_symbol) cout << t.first << ' ' << t.second << endl;
+
+    //消无用式子
     /*
-    cout << "\n"
-         << state_number.size() << "\n\n";
-    for (auto str : state_number) {
-        cout << str.first << ' ' << str.second << endl;
-    }
-    cout << "\n";
-    */
-
-    for (auto t : state_number) {  //存入 node[]
-        node[t.second].val = t.first;
-    }
-
-    //初始判断key所代表的状态是否可达 然后 DFS
-    //也就是判断是否到达状态有全为终结符的状态
-    for (auto key : all_production) {
-        auto to_state = key.second;
-        for (auto t : to_state) {
-            bool flag = true;
-            for (auto c : t) {
-                if (c >= 'A' && c <= 'Z') {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                node[state_number[t]].reachable = true;
-                node[state_number[key.first]].reachable = true;
-                // break;
+    for (auto production : all_production) {
+        auto start = production.first;
+        auto to_state_vec = production.second;
+        for (auto to_state : to_state_vec) {
+            for (auto c : to_state) {
+                if (c >= 'A' && c <= 'Z')
+                    reachable[start_symbol[start]] |= dfs(start_symbol[string(1, c)]);
             }
         }
     }
+    */
+    // cout << start_symbol.size() << endl;
+    // cout << start_symbol_rev.size() << endl;
 
-    for (int i = 1; i <= state_number.size(); i++) cout << node[i].val << ' ' << node[i].reachable << endl;
+    if (!dfs(start_symbol["S"])) {
+        cout << "The final production has nothing!\n\n"; //化简后全为空，没有可达式子
+    }
+    else {
+        for (int i = 1; i < number; i++)
+            cout << start_symbol_rev[i] << ' ' << reachable[i] << endl;
+        //删去 all_production 中的所有含有不可达起始符的式子
 
-    //消去不会产生结束符号的递归式子: A->aaaA, A->Aaaa
-    //这样好像有点麻烦，考虑在图上直接dfs所有可达的点，可达就是路径能到全是终结符的字符串，或者 epsilon
-    //这种dfs可以通用处理所有是否可达情况,产生式右边如果有一项可达那就是可达，否则不可达
-    //但是图上点的权值是一个字符串...考虑如何封装为 Node 然后 dfs 求出可达性
-
-    //每个点代表的状态都 DFS 一次 O(T^2) 图不一定是联通的，有的点没有入边
-    for (int i = 1; i <= state_number.size(); i++) {
-        if (node[i].reachable == true) dfs(i);
     }
 
-    puts("----------------");
-    puts("可达性DFS结果如下:");
-    for (int i = 1; i <= state_number.size(); i++) cout << node[i].val << ' ' << node[i].reachable << endl;
-
-    // system("pause");
 }
 
 int main() {
